3e5776f1faf2f7e8e0c90b536dad0a157af9b789
Queso <jhonatandiazgalindo@gmail.com>
2024-07-16 10:29:05 -0500

    asdfmovies

--- prueba3.py
+++ prueba3.py
@@ -11,7 +11,7 @@ plt.subplot(1, 2, 1) #Filas, Columnas, Index
 font1 = {'family':'serif','color':'red','size':40}
 font2 = {'family':'serif','color':'blue','size':10}
 
-plt.plot(x,y,"#4CAF50",linewidth = '2.5')
+plt.plot(x,y,"#8577cb",linewidth = '2.5')
 plt.legend(["Log2(x)"])
 plt.title("Log2(x)", fontdict = font2)
 plt.xlabel("x", fontdict = font2)
@@ -19,7 +19,7 @@ plt.ylabel("Log2(x)", fontdict = font2)
 plt.grid()
 
 plt.subplot(1, 2, 2)
-plt.plot(x,y2,"#8577cb",linewidth = '2')
+plt.plot(x,y2,"#4CAF50",linewidth = '2')
 plt.legend(["Tan(x)"])
 plt.title("Tan(x)", fontdict = font2)
 plt.xlabel("x", fontdict = font2)
