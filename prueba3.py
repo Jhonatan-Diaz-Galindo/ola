2ff688f8d8a524fe4cffa27e7dba52af533b1990
Queso <jhonatandiazgalindo@gmail.com>
2024-07-16 09:59:14 -0500

    Version_05_grafica_mejorada

--- prueba3.py
+++ prueba3.py
@@ -1,7 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-5,5,100)
-y = 1/x
-plt.plot(x,y,"#F46112--")
+x = np.linspace(1,10,100)
+y = np.log2(x)
+y2 = np.tan(x)
+
+
+plt.subplot(1, 2, 1) #Filas, Columnas, Index
+
+font1 = {'family':'serif','color':'red','size':40}
+font2 = {'family':'serif','color':'blue','size':10}
+
+plt.plot(x,y,"#4CAF50",linewidth = '2.5')
+plt.legend(["Log2(x)"])
+plt.title("Log2(x)", fontdict = font2)
+plt.xlabel("x", fontdict = font2)
+plt.ylabel("Log2(x)", fontdict = font2)
+plt.grid()
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y2,"#8577cb",linewidth = '2')
+plt.legend(["Tan(x)"])
+plt.title("Tan(x)", fontdict = font2)
+plt.xlabel("x", fontdict = font2)
+plt.ylabel("Tan(x)", fontdict = font2)
+plt.suptitle("Graficas :D")
+plt.tight_layout()
+plt.grid()
 plt.show()
